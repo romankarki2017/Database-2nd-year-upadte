c43d808f59b69aedfc892e6f843ea63a2b345f07
Roman Karki <romankarki2017@gmail.com>
2026-04-07 09:29:34 +0545

    added insert statement

--- week-2.sql
+++ week-2.sql
@@ -1,62 +1,69 @@
 -- Contact Details
 CREATE TABLE ContactDetails (
-    Phone_no VARCHAR2(15),
-    email VARCHAR2(15),
-    Social_links VARCHAR2(50)
+    phone_no VARCHAR2(20),
+    email VARCHAR2(50),
+    social_links VARCHAR2(100)
 );
 
-ALTER TABLE ContactDetails
-ADD contact_id INT;
+ALTER TABLE ContactDetails ADD contact_id NUMBER;
+ALTER TABLE ContactDetails ADD CONSTRAINT pk_contact PRIMARY KEY (contact_id);
 
-ALTER TABLE ContactDetails
-ADD CONSTRAINT pk_contact PRIMARY KEY (contact_id);
+INSERT INTO ContactDetails (contact_id, phone_no, email, social_links)
+VALUES (1, '9841234567', 'romankarki2017@gmail.com', 'roman2017fb.com');
+
 
 -- Education
-CREATE TABLE Education (
-    institution VARCHAR2(15),
-    year NUM,
-    GPA VARCHAR2(50),
-    stream VARCHAR2(15)
+CREATE TABLE Educations (
+    institution VARCHAR2(100),
+    grad_year NUMBER,
+    GPA VARCHAR2(10),
+    stream VARCHAR2(50)
 );
 
-ALTER TABLE Education
-ADD education_id INT;
+ALTER TABLE Educations ADD education_id NUMBER;
+ALTER TABLE Educations ADD CONSTRAINT pk_education PRIMARY KEY (education_id);
 
-ALTER TABLE Education
-ADD CONSTRAINT pk_education PRIMARY KEY (education_id);
+INSERT INTO Educations (education_id, institution, grad_year, GPA, stream)
+VALUES (101, 'Nami', 2028, '4.0', 'Computing');
 
 
 -- Address
 CREATE TABLE Address (
-    Address_no INT,
-    provience VARCHAR2(15),
+    province VARCHAR2(50),
     district VARCHAR2(50),
-    Ward_no INT
+    Ward_no NUMBER
 );
 
-ALTER TABLE Address
-ADD CONSTRAINT pk_address PRIMARY KEY (Address_no);
+ALTER TABLE Address ADD Address_no NUMBER;
+ALTER TABLE Address ADD CONSTRAINT pk_address PRIMARY KEY (Address_no);
+
+INSERT INTO Address (Address_no, province, district, Ward_no)
+VALUES (501, 'Bagmati', 'Budhanilkantha', 9);
+
 
 -- Identity
 CREATE TABLE Identity (
-    NID_no INT,
-    nationality VARCHAR2(15),
+    nationality VARCHAR2(30),
     country VARCHAR2(50),
     gender VARCHAR2(15)
 );
 
-ALTER TABLE Identity
-ADD CONSTRAINT pk_identity PRIMARY KEY (NID_no);
+ALTER TABLE Identity ADD NID_no NUMBER;
+ALTER TABLE Identity ADD CONSTRAINT pk_identity PRIMARY KEY (NID_no);
+
+INSERT INTO Identity (NID_no, nationality, country, gender)
+VALUES (29291901, 'Nepali', 'Nepal', 'Male');
+
 
 -- Profession
 CREATE TABLE Profession (
-    Position VARCHAR2(15),
-    experience NUM,
-    salary DECIMAL(10,2)
+    position VARCHAR2(50),
+    experience NUMBER,
+    salary NUMBER(10,2)
 );
 
-ALTER TABLE Profession
-ADD profession_id INT;
+ALTER TABLE Profession ADD profession_id NUMBER;
+ALTER TABLE Profession ADD CONSTRAINT pk_profession PRIMARY KEY (profession_id);
 
-ALTER TABLE Profession
-ADD CONSTRAINT pk_profession PRIMARY KEY (profession_id);
+INSERT INTO Profession (profession_id, position, experience, salary)
+VALUES (1232, 'Software Engineer', 2, 999999.00);
