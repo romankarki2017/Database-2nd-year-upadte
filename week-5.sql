612f83616f02b3abfcca3c5bb1f6ae1879e2a79e
Roman Karki <romankarki2017@gmail.com>
2026-04-19 10:51:07 +0545

    Implemented SIMPLE VARRAY and MULTIDIMENSIONAL VARRAY using object types

--- week-5.sql
+++ week-5.sql
@@ -21,7 +21,7 @@ CREATE OR REPLACE TYPE object_type_name AS OBJECT(
 )
 
 -- STEP 2: uses as an object table definations
-CREATE TABLE object_table_name OF object_tyoe_name;
+CREATE TABLE object_table_name OF object_type_name;
 
 -- STEP 3:
 CREATE TABLE TABLE_NAME(
@@ -30,7 +30,7 @@ CREATE TABLE TABLE_NAME(
     attribute REF object_type_name
     SCOPE IS object_table_name);
 
------
+-----  MY TABLE AND OBJECT TYPE EXAMPLE
 CREATE OR REPLACE TYPE student_type AS OBJECT (
     student_id   NUMBER(6),
     student_name VARCHAR2(50),
@@ -48,3 +48,35 @@ CREATE TABLE instructors(
     instructor_id NUMBER(6),
     address REF address_type SCOPE IS address_table
 );
+
+-- SIMPLE VARRAY
+CREATE TYPE array_type_name AS
+VARRAY(SIZE) OF datatype;
+
+CREATE TYPE name_varray_type AS 
+VARRAY(5) OF VARCHAR2(50);
+/
+
+
+-- MULTIDIMENSIONAL VARRAY
+--Step 1: create type
+CREATE TYPE object_type_name AS OBJECT(
+    attribute_name DATATYPE(SIZE),
+    attribute_name DATATYPE(SIZE),
+    attribute_name DATATYPE(SIZE),
+)
+
+--step 2: use the type defination to create the varray
+CREATE TYPE varray_type_name AS 
+VARRAY(SIZE) OF object_type_name;
+/
+
+--USING VARRAY
+CREATE TABLE sites (
+    site_id NUMBER(6),
+    classroom Classromm_varray_type
+);
+
+ALTER TABLE sites
+ADD (classroom Classromm_varray_type);
+
