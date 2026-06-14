62b88e6042b26a9d9a01dfd4c710355637ea728a
Roman Karki <romankarki2017@gmail.com>
2026-06-14 07:25:30 +0545

    Add PL/SQL function creation and execution examples

--- week-11.sql
+++ week-11.sql
@@ -67,3 +67,76 @@ BEGIN
     RETURN vn_course_ct;
 
 END fucn_count_offerings;
+
+
+
+CREATE OR REPLACE FUNCTION get_total_students 
+RETURN NUMBER IS
+   vn_total NUMBER;
+BEGIN
+   SELECT COUNT(*) 
+   INTO vn_total 
+   FROM students;
+   
+   RETURN vn_total;
+END get_total_students;
+/
+
+-- CALLING FUNCTION. EXAMPLE
+CREATE OR REPLACE PROCEDURE
+proc_using_func IS
+vn_no_runs number(20);
+
+BEGIN
+
+vn_no_of_run:= func_count_offering;
+DBMS_OUTPUT.PUT LINE ('the value returned from the function is' || vn_no_of_runs);
+
+END proc_using_func;
+/
+
+-- PARAMETER IN FUNCTIONS
+CREATE OR REPLACE FUNCTION fuc_count_offerings
+(in_course_id NUMBER) RETURN number IS
+vn_course_ct NUMBER(4);
+
+BEGIN
+    SELECT COUNT(*)
+    INTO vn_course_ct
+    FROM offerings
+    WHERE course_id = in_course_id;
+
+    RETURN vn_course_ct;
+
+END func_count_offerings;
+/
+
+-- Calling Function With parameter
+CREATE OR REPLACE PROCEDURE
+proc_using_func(in_course_id NUMBER)IS
+
+vn_no_of_NUMBERS(20);
+BEGIN
+
+vn_no_of_runs:=
+func_count_offerings(in_course_id);
+
+END proc_using_func;
+
+-- DROPPING FUNCTION
+DROP FUNCTION function_name;
+DROP FUNCTION func_count_staff;
+
+-------------
+CREATE OR REPLACE FUNCTION fn_username (
+   firstname IN VARCHAR2,
+   lastname  IN VARCHAR2
+) RETURN VARCHAR2 IS
+   username VARCHAR2(20);
+BEGIN
+   
+   username := SUBSTR(firstname, 1, 2) || SUBSTR(lastname, 1, 5);
+   
+   RETURN username;
+END fn_username;
+/
